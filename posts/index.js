aa35dc6c88dadf434281bf37d1759789eee84ad5
Bikash Mishra <mishra.bikash002@gmail.com>
2021-04-07 10:55:30 +0530

    created different kubernetes objects & modified posts-depl.yaml

--- posts/index.js
+++ posts/index.js
@@ -19,10 +19,11 @@ app.post('/posts', async (req, res) => {
     const { title } = req.body;
 
     posts[id] = {
-        id, title
+        id,
+        title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
